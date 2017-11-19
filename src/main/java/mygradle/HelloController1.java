4037ac3bce1f6d09ba1eabbb6002f5c107435603
xiaopeng <18971903450@163.com>
2017-11-19 10:29:10 +0800

    2016年7月22日13:33:45

--- src/main/java/mygradle/HelloController1.java
+++ src/main/java/mygradle/HelloController1.java
@@ -17,15 +17,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 
 /**
  * Created by miaorf on 2016/6/19.
  */
 @RestController
-public class HelloController {
-    @RequestMapping(value = "/XQCenter/servlet/GetSBP", method = RequestMethod.GET)
+public class HelloController1 {
+    @RequestMapping(value = "/NYControl/servlet/GetSBP", method = RequestMethod.GET)
     public String test(HttpServletRequest request, HttpServletResponse response) {
         System.out.println(request.getRequestURL());
         Map<String, String> urlkeydare=URLRequest(request.getQueryString());
@@ -42,39 +45,42 @@ public class HelloController {
         System.out.println(json);
         return json;
     }
-    @RequestMapping(value = "/XQCenter/servlet/updatecheck", method = RequestMethod.GET)
+    @RequestMapping(value = "/NYControl/servlet/updatecheck", method = RequestMethod.GET)
     public String test2(HttpServletRequest request, HttpServletResponse response) {
         System.out.println(request.getRequestURL());
         System.out.println(request.getQueryString());
 
         response.setHeader("Accept-Charset", "big5, big5-hkscs, compound_text, euc-jp, euc-kr, gb18030, gb2312, gbk, ibm-thai, ibm00858, ibm01140, ibm01141, ibm01142, ibm01143, ibm01144, ibm01145, ibm01146, ibm01147, ibm01148, ibm01149, ibm037, ibm1026, ibm1047, ibm273, ibm277, ibm278, ibm280, ibm284, ibm285, ibm297, ibm420, ibm424, ibm437, ibm500, ibm775, ibm850, ibm852, ibm855, ibm857, ibm860, ibm861, ibm862, ibm863, ibm864, ibm865, ibm866, ibm868, ibm869, ibm870, ibm871, ibm918, iso-2022-cn, iso-2022-jp, iso-2022-jp-2, iso-2022-kr, iso-8859-1, iso-8859-13, iso-8859-15, iso-8859-2, iso-8859-3, iso-8859-4, iso-8859-5, iso-8859-6, iso-8859-7, iso-8859-8, iso-8859-9, jis_x0201, jis_x0212-1990, koi8-r, koi8-u, shift_jis, tis-620, us-ascii, utf-16, utf-16be, utf-16le, utf-32, utf-32be, utf-32le, utf-8, windows-1250, windows-1251, windows-1252, windows-1253, windows-1254, windows-1255, windows-1256, windows-1257, windows-1258, windows-31j, x-big5-hkscs-2001, x-big5-solaris, x-euc-jp-linux, x-euc-tw, x-eucjp-open, x-ibm1006, x-ibm1025, x-ibm1046, x-ibm1097, x-ibm1098, x-ibm1112, x-ibm1122, x-ibm1123, x-ibm1124, x-ibm1381, x-ibm1383, x-ibm33722, x-ibm737, x-ibm833, x-ibm834, x-ibm856, x-ibm874, x-ibm875, x-ibm921, x-ibm922, x-ibm930, x-ibm933, x-ibm935, x-ibm937, x-ibm939, x-ibm942, x-ibm942c, x-ibm943, x-ibm943c, x-ibm948, x-ibm949, x-ibm949c, x-ibm950, x-ibm964, x-ibm970, x-iscii91, x-iso-2022-cn-cns, x-iso-2022-cn-gb, x-iso-8859-11, x-jis0208, x-jisautodetect, x-johab, x-macarabic, x-maccentraleurope, x-maccroatian, x-maccyrillic, x-macdingbat, x-macgreek, x-machebrew, x-maciceland, x-macroman, x-macromania, x-macsymbol, x-macthai, x-macturkish, x-macukraine, x-ms932_0213, x-ms950-hkscs, x-ms950-hkscs-xp, x-mswin-936, x-pck, x-sjis_0213, x-utf-16le-bom, x-utf-32be-bom, x-utf-32le-bom, x-windows-50220, x-windows-50221, x-windows-874, x-windows-949, x-windows-950, x-windows-iso2022jp");
         String json1="{\"order\":\"0\",\"res2\":{\"url\":\"xq.hh108.com/hc/ailiyou/res/Resource2/10001/hc_bundle2_10001_20160805172944.zip\",\"ver\":\"10001\",\"must\":\"1\",\"size\":\"20487193\",\"type\":\"4\",\"md5\":\"f87b266ca6a33c3c1d9c2b2d27be4be1\"}}";
-        if(request.getQueryString().contains("res_v2=10001"))
+        if(request.getQueryString().contains("es_v2=0"))
         {
             json1="{\"order\":\"0\"}";
         }
         System.out.println(json1);
         return json1;
     }
-    @RequestMapping(value = "/XQCenter/servlet/LoginLog333", method = RequestMethod.GET)
+    @RequestMapping(value = "/NYControl/servlet/LoginLog333", method = RequestMethod.GET)
     public String test4(HttpServletRequest request, HttpServletResponse response) {
-        java.util.Random random=new java.util.Random();
+        Random random=new Random();
         int result=random.nextInt(10)*random.nextInt(10)+random.nextInt(10)+random.nextInt(10)*random.nextInt(10);
         String json = result+"";
         System.out.println(json);
         return json;
     }
-    @RequestMapping(value = "/XQCenter/login/*", method = RequestMethod.GET)
+    @RequestMapping(value = "/NYControl/login/*", method = RequestMethod.GET)
     public String test5(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println(request.getRequestURL());
+        System.out.println(request.getQueryString());
         Map<String, String> urlkeydare=URLRequest(request.getQueryString());
-        String json=get("http://xqcenter.7j123.cn/XQCenter/login/yijie?os=2&sdk=%7B841B5068-E48AB443%7D&sub=yijie&app=%7BF0489371-1767F93D%7D&sess=NTkwYzVlYTBwanJrc3ZnMTBmYTlybXMzazM&uin="+urlkeydare.get("uin"),null);
+        String uin = urlkeydare.get("uin");
+        String json=get("http://gdv1.mhjqb.jinkehc.com:8080/NYControl/login/yijieguandu?os=2&sdk=EE76A3092912D890&sub=maxvip&app=E2AFC9D1292B0500&uin="+urlkeydare.get("uin")+"&sess=OWNjNTFjNWM0NzZlN2Q2ODFhZTg2OWMzZWQwNDNmZDg&",null);
         System.out.println(json);
         return json;
     }
-    @RequestMapping(value = "/XQCenter/servlet/GetUpdateNoticeServlet", method = RequestMethod.GET)
+    @RequestMapping(value = "/NYControl/servlet/GetUpdateNoticeServlet", method = RequestMethod.GET)
     public String test6(HttpServletRequest request, HttpServletResponse response) {
         Map<String, String> urlkeydare=URLRequest(request.getQueryString());
-        String json="{\"notice_dt\":\"2017年11月17日\",\"popup\":1,\"content\":\"#000000公告：\\r\\n《梦幻猫咪》猫咪老师出品QQ593619428,进Q群118101993体验\\r\\n\\r\\n福利介绍：\\r\\n#FF0000\\r\\n1、充值比例1：1000，首次充值双倍水玉返还\\r\\n2、上线即送首充，100W水玉，999W银币\\r\\n3、首充赠送神兽、40级无级别极品武器，40级橙色套装\\r\\n4、每日活跃送水玉，40、100活跃分别可领取500、1000水玉\\r\\n5、七日登录奖励大幅提升，次日送珍兽，五日送坐骑，7天累计10000水玉\\r\\n7、等级礼包奖励升级，靓丽时装免费送，累计可领取12万水玉\\r\\n8.战力争霸赛：战力第一获得战力第一礼包，内含紫兽*1、80无级别红色衣服*1\\r\\n9.最强帮派：争夺最强帮派后，整个帮的所有成员都将获得丰厚奖励，职位越高，奖励越好\\r\\n10.全服冲级：欢乐冲级拿水玉，50万元宝等你来拿!等级第一获得水玉*500000，第二获得水玉*200000，第三获得水玉*100000\\r\\n\\r\\n#000000防盗防骗提醒\\r\\n#FF0000为保障玩家账号安全，官方不推荐任何形式的线下交易，任何线下交易均存在安全隐患，尽量避免您的利益受到损失。请您不要轻易相信线下交易行为，感谢您的配合，祝您游戏愉快。\\r\\n\",\"end_time\":3910156,\"show_time\":100000}";
+        String json="{\"notice_dt\":\"2017年11月16日\",\"popup\":1,\"content\":\"#000000删档内测公告：\\r\\n《梦幻猫咪》将于11月16日12：00~11月22日18：00开启删档内测，凡是在内测中充值的玩家，公测时不仅全额返还充值，还可以额外享受双倍返利\\r\\n\\r\\n福利介绍：\\r\\n#FF0000\\r\\n1、充值比例1：1000，首次充值双倍水玉返还\\r\\n2、上线即送至尊VIP（满V），28888水玉，999W银币\\r\\n3、首充赠送神兽、40级无级别极品武器，40级橙色套装\\r\\n4、每日活跃送水玉，40、100活跃分别可领取500、1000水玉\\r\\n5、七日登录奖励大幅提升，次日送珍兽，五日送坐骑，7天累计10000水玉\\r\\n7、等级礼包奖励升级，靓丽时装免费送，累计可领取12万水玉\\r\\n8.战力争霸赛：战力第一获得战力第一礼包，内含紫兽*1、80无级别红色衣服*1\\r\\n9.最强帮派：争夺最强帮派后，整个帮的所有成员都将获得丰厚奖励，职位越高，奖励越好\\r\\n10.全服冲级：欢乐冲级拿水玉，50万元宝等你来拿!等级第一获得水玉*500000，第二获得水玉*200000，第三获得水玉*100000\\r\\n\\r\\n#000000防盗防骗提醒\\r\\n#FF0000为保障玩家账号安全，官方不推荐任何形式的线下交易，任何线下交易均存在安全隐患，尽量避免您的利益受到损失。请您不要轻易相信线下交易行为，感谢您的配合，祝您游戏愉快。\\r\\n\",\"end_time\":3910156,\"show_time\":100000}";
         System.out.println(json);
         return json;
     }
